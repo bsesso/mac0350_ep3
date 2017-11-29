b8eb08061bedb82acd30fb7570417cafbba4f681
Bruno <bruno.sesso@usp.br>
2017-11-29 20:54:52 -0200

    Implementei o form para criação de usuário
    Implementei o form de criação do usuário com todos os campos que tem no
    modelo do Usuário. Além disso criei um template para uma página
    agradecendo um formulário submetido com sucesso. Atualizei as urls e
    adicionei os devidos métodos na views

--- mac0350_ep3/ep3/urls.py
+++ mac0350_ep3/ep3/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
+	url(r'^create_user/$', views.create_user, name='create_user'),
+	url(r'^thanks/$', views.thanks, name='thanks'),
 ]
--- mac0350_ep3/ep3/views.py
+++ mac0350_ep3/ep3/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 
-def index(request):
-	template = loader.get_template('ep3/index.html')
-	context = {}
-	return HttpResponse(template.render(context, request))
+from ep3.models import UsuarioForm
 
+def index(request):
+	if not request.method == 'POST':
+		template = loader.get_template('ep3/index.html')
+		form = UsuarioForm()
+		context = {'form': form}
+		return HttpResponse(template.render(context, request))
+	return None
+
+def create_user(request):
+	form = None
+	if request.method == 'POST':
+		form = UsuarioForm(request.POST)
+		if form.is_valid():
+			return HttpResponseRedirect('/ep3/thanks/')
+	else:
+		form = UsuarioForm
+
+	template_path = 'ep3/create_user.html'
+	context = {'form': form}
+	return render(request, template_path, context)
+
+def thanks(request):
+	template_path = 'ep3/thanks.html'
+	context = {}
+
+	return render(request, template_path, context)
